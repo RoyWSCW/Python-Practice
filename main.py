f24ebf38f6888c3f57445154f14cbb36104c8e86
RoyWSCW <xur@wsc.school.nz>
2024-05-02 12:14:21 +1200

    Quiz made

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 
 # Ask the user their name and save it
+input("Hello, what's your name?")
 
 # Greet the user and introduce the quiz
+print("Welcome to the Roy quiz")
 
 # Ask the user a question
+input("What is Roy's favourite colour? Red, blue, green or yellow?")
 
 # Tell them the correct answer
+print("The correct answer was Green!")
 
 # End the quiz
+print("It's now the end of the quiz, thanks for playing.")
