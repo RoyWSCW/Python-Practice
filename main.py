77b8252723890bc5f8885d3067de80a6aa1506e3
RoyWSCW <xur@wsc.school.nz>
2024-05-02 12:29:37 +1200

    upd

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 
 # Ask the user their name and save it
-input("Hello, what's your name?")
+NAME = input("Hello, what's your name?")
 
 # Greet the user and introduce the quiz
-print("Welcome to the Roy quiz")
+print("Welcome to the science quiz", NAME)
 
 # Ask the user a question
-input("What is Roy's favourite colour? Red, blue, green or yellow?")
+input("What is Carbon's atomic number?")
 
 # Tell them the correct answer
-print("The correct answer was Green!")
+print("The correct answer was 6")
 
 # End the quiz
 print("It's now the end of the quiz, thanks for playing.")
