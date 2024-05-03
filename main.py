f232c872c744125c86122787ebe8bbff95820303
RoyWSCW <xur@wsc.school.nz>
2024-05-03 13:00:09 +1200

    v2

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 
 # Ask the user their name and save it
-NAME = input("Hello, what's your name?")
+name = input("Hello, what's your name?")
 
 # Greet the user and introduce the quiz
-print("Welcome to the science quiz", NAME)
+print("Welcome to the science quiz",name)
 
 # Ask the user a question
-input("What is Carbon's atomic number?")
+answer = input("What is Carbon's atomic number?")
 
-# Tell them the correct answer
-print("The correct answer was 6")
-
+# Check the user’s answer and give feedback
+if answer == "6":
+    print("Correct!")
+elif answer == "":
+    print("Not sure?")
+else:
+    print("Wrong!")
+print("The answer is 6")
+    
 # End the quiz
 print("It's now the end of the quiz, thanks for playing.")
