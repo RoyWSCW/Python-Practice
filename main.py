4e4ceb7d7faf84742dabb7fa90298fe6b962952e
RoyWSCW <xur@wsc.school.nz>
2024-05-16 11:44:03 +1200

    added loops to all

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ while True:
             # Ask the user a question
             answer = input("Q.1 What is Carbon's atomic number?\n")
 
-                # Check the user’s answer and give feedback
+            # Check the user’s answer and give feedback
             if answer == "6" or answer == " 6":
                 print("Correct!")
                 score += 1
@@ -40,129 +40,172 @@ while True:
 
             question_attempts -= 1
 
-        answer = input("Q.2 True or False the element Argon is a noble gas\n")
-        answer = answer.lower()
+        question_attempts = tries
+        while question_attempts > 0:
+            answer = input("Q.2 True or False the element Argon is a noble gas\n")
+            answer = answer.lower()
 
-        if answer == "true" or answer == " true" or answer == "t" or answer == " t":
-            print("Correct!")
-            score += 1 
-        elif answer == "false" or answer == " false" or answer == "f" or answer == " f":
-            print("Incorrect!")
-        elif answer == "":
-            print("Not sure?")
+            if answer == "true" or answer == " true" or answer == "t" or answer == " t":
+                print("Correct!")
+                score += 1
+                break 
+            elif answer == "false" or answer == " false" or answer == "f" or answer == " f":
+                print("Incorrect!")
+            elif answer == "":
+                print("Not sure?")
+            
+            question_attempts -= 1
 
-        answer = input("Q.3 What is the name of the 12th element on the periodic table?\n")
-        answer = answer.lower()
+        question_attempts = tries
+        while question_attempts > 0:
+            answer = input("Q.3 What is the name of the 12th element on the periodic table?\n")
+            answer = answer.lower()
 
-        if answer == "magnesium" or answer == "mg":
-            print("Correct!")
-            score += 1
-        elif answer == "":
-            print("Not sure?")
-        else:
-            print("Wrong!")
-            print("The answer is Magnesium")
+            if answer == "magnesium" or answer == "mg":
+                print("Correct!")
+                score += 1
+                break
+            elif answer == "":
+                print("Not sure?")
+            else:
+                print("Wrong!")
+                print("The answer is Magnesium")
 
+            question_attempts -=1
+            
 
-        answer = input("Q.4 What element does the symbol Au on the periodic table represent?\n")
-        answer = answer.lower()
+        question_attempts = tries
+        while question_attempts > 0:
+            answer = input("Q.4 What element does the symbol Au on the periodic table represent?\n")
+            answer = answer.lower()
 
-        if answer == "gold" or answer == " gold":
-            print("Correct!")
-            score += 1
-        elif answer == "":
-            print("Not sure?")
-        else:
-            print("Wrong!")
-            print("The answer is Gold")
+            if answer == "gold" or answer == " gold":
+                print("Correct!")
+                score += 1
+                break
+            elif answer == "":
+                print("Not sure?")
+            else:
+                print("Wrong!")
+                print("The answer is Gold")
 
-        answer = input("Q.5 True or False the letter Q is not used in the symbol of any element on the periodic table\n")
-        answer = answer.lower()
+            question_attempts -= 1
 
-        if answer == "true" or answer == " true" or answer == "t" or answer == " t":
-            print("Correct!")
-            score += 1 
-        elif answer == "false" or answer == " false" or answer == "f" or answer == " f":
-            print("Incorrect!")
-        elif answer == "":
-            print("Not sure?")
+        question_attempts = tries
+        while question_attempts > 0:
+            answer = input("Q.5 True or False the letter Q is not used in the symbol of any element on the periodic table\n")
+            answer = answer.lower()
 
-        question = "Q.6 What element does the symbol Ni represent?"
-        a = "Nitrogen"
-        b = "Nickel"
-        c = "Nihonium"
-        d = "Niobium"
-        answer = input(QUESTION_FORMAT.format(question, a, b, c, d)).lower()
+            if answer == "true" or answer == " true" or answer == "t" or answer == " t":
+                print("Correct!")
+                score += 1
+                break 
+            elif answer == "false" or answer == " false" or answer == "f" or answer == " f":
+                print("Incorrect!")
+            elif answer == "":
+                print("Not sure?")
 
-        if answer == b.lower() or answer == "b":
-            print("Correct!")
-            score += 1
-        elif answer == "":
-            print("Not sure?")
-        elif answer != a and answer != "a" and answer != b and answer != "b" and answer != c and answer != "c" and answer != d and answer != "d":
-            print("That wasn't an option")
-        else:
-            print("Wrong!")
-            print("The answer is Nickel")
+            question_attempts -= 1
 
-        answer = input("Q.7 What is sodium's symbol on the periodic table?\n")
-        answer = answer.lower()
+        question_attempts = tries
+        while question_attempts > 0:
+            question = "Q.6 What element does the symbol Ni represent?"
+            a = "Nitrogen"
+            b = "Nickel"
+            c = "Nihonium"
+            d = "Niobium"
+            answer = input(QUESTION_FORMAT.format(question, a, b, c, d)).lower()
 
-        if answer == "na" or answer == " na":
-            print("Correct!")
-            score += 1
-        elif answer == "":
-            print("Not sure?")
-        else:
-            print("Incorrect!")
-            print("The answer is Na")
+            if answer == b.lower() or answer == "b":
+                print("Correct!")
+                score += 1
+                break
+            elif answer == "":
+                print("Not sure?")
+            elif answer != a and answer != "a" and answer != b and answer != "b" and answer != c and answer != "c" and answer != d and answer != "d":
+                print("That wasn't an option")
+            else:
+                print("Wrong!")
+                print("The answer is Nickel")
 
-        question = "Q.8 How many elements are on the periodic table?"
-        a = "117"
-        b = "118"
-        c = "119"
-        d = "120"
-        answer = input(QUESTION_FORMAT.format(question, a, b, c, d)).lower()
+            question_attempts -= 1
 
-        if answer == b or answer == "b" or answer == " 118" or answer == "118":
-            print("Correct!")
-            score += 1
-        elif answer == "":
-            print("Not sure?")
-        elif answer != a and answer != "a" and answer != b and answer != "b" and answer != c and answer != "c" and answer != d and answer != "d":
-            print("That wasn't an option")
-        else:
-            print("Wrong!")
-            print("The answer is 118")
+        question_attempts = tries
+        while question_attempts > 0:
+            answer = input("Q.7 What is sodium's symbol on the periodic table?\n")
+            answer = answer.lower()
 
-        answer = input("Q.9 True or False the letter J is not used in the symbol of any element on the periodic table\n")
-        answer = answer.lower()
+            if answer == "na" or answer == " na":
+                print("Correct!")
+                score += 1
+                break
+            elif answer == "":
+                print("Not sure?")
+            else:
+                print("Incorrect!")
+                print("The answer is Na")
 
-        if answer == "true" or answer == " true" or answer == "t" or answer == " t":
-            print("Correct!")
-            score += 1 
-        elif answer == "false" or answer == " false" or answer == "f" or answer == " f":
-            print("Incorrect!")
-        elif answer == "":
-            print("Not sure?")
+            question_attempts -= 1
 
-        question = "Q.10 Is the person who made this quiz a better coder than you, for this specific question please only answer in one letter"
-        a = "yes"
-        b = "no, just kidding of course yes"
-        c = "yes because im a bad coder"
-        d = "Opposite of no (yes)"
-        answer = input(QUESTION_FORMAT.format(question, a, b, c, d)).lower()
+        question_attempts = tries
+        while question_attempts > 0:
+            question = "Q.8 How many elements are on the periodic table?"
+            a = "117"
+            b = "118"
+            c = "119"
+            d = "120"
+            answer = input(QUESTION_FORMAT.format(question, a, b, c, d)).lower()
 
-        if answer == a and answer == "a" and answer == b and answer == "b" and answer == c and answer == "c" and answer == d and answer == "d":
-            print("Correct!")
-            score += 1
-        elif answer == "":
-            print("Not sure?")
-        elif answer != a and answer != "a" and answer != b and answer != "b" and answer != c and answer != "c" and answer != d and answer != "d":
-            print("That wasn't an option")
-        else:
-            print("Wrong!")
-            print("The answer is any of the above and you know it")
+            if answer == b or answer == "b" or answer == " 118" or answer == "118":
+                print("Correct!")
+                score += 1
+                break
+            elif answer == "":
+                print("Not sure?")
+            elif answer != a and answer != "a" and answer != b and answer != "b" and answer != c and answer != "c" and answer != d and answer != "d":
+                print("That wasn't an option")
+            else:
+                print("Wrong!")
+                print("The answer is 118")
+
+            question_attempts -= 1
+
+        question_attempts = tries
+        while question_attempts > 0:
+            answer = input("Q.9 True or False the letter J is not used in the symbol of any element on the periodic table\n")
+            answer = answer.lower()
+
+            if answer == "true" or answer == " true" or answer == "t" or answer == " t":
+                print("Correct!")
+                score += 1
+                break 
+            elif answer == "false" or answer == " false" or answer == "f" or answer == " f":
+                print("Incorrect!")
+            elif answer == "":
+                print("Not sure?")
+
+        question_attempts = tries
+        while question_attempts > 0:
+            question = "Q.10 Is the person who made this quiz a better coder than you, for this specific question please only answer in one letter"
+            a = "yes"
+            b = "no, just kidding of course yes"
+            c = "yes because im a bad coder"
+            d = "Opposite of no (yes)"
+            answer = input(QUESTION_FORMAT.format(question, a, b, c, d)).lower()
+
+            if answer == a and answer == "a" and answer == b and answer == "b" and answer == c and answer == "c" and answer == d and answer == "d":
+                print("Correct!")
+                score += 1
+                break
+            elif answer == "":
+                print("Not sure?")
+            elif answer != a and answer != "a" and answer != b and answer != "b" and answer != c and answer != "c" and answer != d and answer != "d":
+                print("That wasn't an option")
+            else:
+                print("Wrong!")
+                print("The answer is any of the above and you know it")
+
+            question_attempts -= 1
 
         # End the quiz
         if score < 9:
